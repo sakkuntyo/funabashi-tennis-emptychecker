0e3d54f363608d8f31780ee6124e5b05b995545e
sakkuntyo <20591351+sakkuntyo@users.noreply.github.com>
2022-11-20 23:06:13 +0900

    while削除

--- funabashi-yoyakulist.js
+++ funabashi-yoyakulist.js
@@ -14,70 +14,68 @@ const lineNotifyToken = JSON.parse(fs.readFileSync("./settings.json", "utf8")).l
 const kakoyoyakuList = [];
 
 (async () => {
-  while(true){
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox']//,
-      //headless: false
-    });
-    try{
-      const page = await browser.newPage();
-      page.on('dialog',async dialog => {
-        dialog.accept();
-      })
-      await page.goto('https://funayoyaku.city.funabashi.chiba.jp/web/index.jsp');
-      await page.waitForFunction(()=> document.readyState === "complete");  
-    
-      await page.type('input[id="userId"]',JSON.parse(fs.readFileSync("./settings.json", "utf8")).userid);
-      await page.waitForFunction(()=> document.readyState === "complete");  
-    
-      await page.type('input[id="password"]',JSON.parse(fs.readFileSync("./settings.json", "utf8")).password);
-      await page.waitForFunction(()=> document.readyState === "complete");  
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox']//,
+    //headless: false
+  });
+  try{
+    const page = await browser.newPage();
+    page.on('dialog',async dialog => {
+      dialog.accept();
+    })
+    await page.goto('https://funayoyaku.city.funabashi.chiba.jp/web/index.jsp');
+    await page.waitForFunction(()=> document.readyState === "complete");  
+  
+    await page.type('input[id="userId"]',JSON.parse(fs.readFileSync("./settings.json", "utf8")).userid);
+    await page.waitForFunction(()=> document.readyState === "complete");  
+  
+    await page.type('input[id="password"]',JSON.parse(fs.readFileSync("./settings.json", "utf8")).password);
+    await page.waitForFunction(()=> document.readyState === "complete");  
 
-      await page.click('img[src="image/bw_login.gif"]');
-      await page.waitForFunction(()=> document.readyState === "complete");  
+    await page.click('img[src="image/bw_login.gif"]');
+    await page.waitForFunction(()=> document.readyState === "complete");  
+  
+    await page.click('img[alt="予約の確認"]');
+    await page.waitForFunction(()=> document.readyState === "complete");  
     
-      await page.click('img[alt="予約の確認"]');
-      await page.waitForFunction(()=> document.readyState === "complete");  
-      
-      //table ヘッダー行削除
-      await page.$eval('tr',el => el.remove());
-      await page.$eval('tr',el => el.remove());
-      await page.$eval('tr',el => el.remove());
-      await page.$eval('tr',el => el.remove());
+    //table ヘッダー行削除
+    await page.$eval('tr',el => el.remove());
+    await page.$eval('tr',el => el.remove());
+    await page.$eval('tr',el => el.remove());
+    await page.$eval('tr',el => el.remove());
 
-      //日時取得
-      const myLine = new Line();
-      yoyakulist = []
-      yoyakucount = ((await page.$x(`//td`)).length) / 10
-      console.log("yoyakucount -> " + yoyakucount)
-      for(let i = 1;i <= yoyakucount;i++){
-        date = (await (await page.$x(`//td`))[1].getProperty('innerText')).toString().replace(/JSHandle:(.*)/g,"$1").replace('\n',' ')
-        jikan = (await (await page.$x(`//td`))[2].getProperty('innerText')).toString().replace(/JSHandle:(.*)/g,"$1") 
-        courtname = (await (await page.$x(`//td`))[3].getProperty('innerText')).toString().replace(/JSHandle:(.*)/g,"$1") 
-        courtnumber = (await (await page.$x(`//td`))[4].getProperty('innerText')).toString().replace(/JSHandle:(.*)/g,"$1") 
-        console.log("date -> " + date)
-        console.log("jikan -> " + jikan)
-        console.log("courtname -> " + courtname)
-        console.log("courtnumber -> " + courtnumber)
-        yoyakulist.push({"日付": date, "時間": jikan, "コート名": courtname, "コート番号": courtnumber})
-        await page.$eval('tr',el => el.remove());
-      }
-      console.log(yoyakulist)
-      myLine.setToken(lineNotifyToken);
-      message = "船橋の予約状況" + "\n"
-      yoyakulist.forEach(yoyaku => message = message + "- "+ yoyaku["日付"] + " " + yoyaku["時間"] + " " + yoyaku["コート名"] + " " + yoyaku["コート番号"] + "\n")
-      message = message + "利用者番号:" + JSON.parse(fs.readFileSync("./settings.json", "utf8")).userid + "\n"
-      message = message + "パスワード:" + JSON.parse(fs.readFileSync("./settings.json", "utf8")).password + "\n"
-      message = message + "url:" + "https://funayoyaku.city.funabashi.chiba.jp/web"
-      myLine.notify(message);
-    
-      await browser.close();
-      await setTimeout(60000);
-    } catch(error) {
-      console.log("catched" + error)
-      console.error(error)
-      await browser.close();
+    //日時取得
+    const myLine = new Line();
+    yoyakulist = []
+    yoyakucount = ((await page.$x(`//td`)).length) / 10
+    console.log("yoyakucount -> " + yoyakucount)
+    for(let i = 1;i <= yoyakucount;i++){
+      date = (await (await page.$x(`//td`))[1].getProperty('innerText')).toString().replace(/JSHandle:(.*)/g,"$1").replace('\n',' ')
+      jikan = (await (await page.$x(`//td`))[2].getProperty('innerText')).toString().replace(/JSHandle:(.*)/g,"$1") 
+      courtname = (await (await page.$x(`//td`))[3].getProperty('innerText')).toString().replace(/JSHandle:(.*)/g,"$1") 
+      courtnumber = (await (await page.$x(`//td`))[4].getProperty('innerText')).toString().replace(/JSHandle:(.*)/g,"$1") 
+      console.log("date -> " + date)
+      console.log("jikan -> " + jikan)
+      console.log("courtname -> " + courtname)
+      console.log("courtnumber -> " + courtnumber)
+      yoyakulist.push({"日付": date, "時間": jikan, "コート名": courtname, "コート番号": courtnumber})
+      await page.$eval('tr',el => el.remove());
     }
+    console.log(yoyakulist)
+    myLine.setToken(lineNotifyToken);
+    message = "船橋の予約状況" + "\n"
+    yoyakulist.forEach(yoyaku => message = message + "- "+ yoyaku["日付"] + " " + yoyaku["時間"] + " " + yoyaku["コート名"] + " " + yoyaku["コート番号"] + "\n")
+    message = message + "利用者番号:" + JSON.parse(fs.readFileSync("./settings.json", "utf8")).userid + "\n"
+    message = message + "パスワード:" + JSON.parse(fs.readFileSync("./settings.json", "utf8")).password + "\n"
+    message = message + "url:" + "https://funayoyaku.city.funabashi.chiba.jp/web"
+    myLine.notify(message);
+  
+    await browser.close();
+    await setTimeout(60000);
+  } catch(error) {
+    console.log("catched" + error)
+    console.error(error)
+    await browser.close();
   }
 })();
 
